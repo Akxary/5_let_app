a4a3f54bb00b704f91f58ef171c78ad0bc9fa203
pollx <pollxx97@gmail.com>
2024-01-23 01:42:15 +0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script>
+export default {
+  
+  data () {
+    return {
+      cnt: 0
+    }
+  },
+
+  methods:{
+    increment() {
+      this.cnt++
+    }
+  }
+}
+</script>
+
+<template>
+  <header>
+    <h2>Hello</h2>
+  </header>
+
+  <main>
+    <h2>HelloWorld</h2>
+    <button class="" @click="increment">Increment is: {{ cnt }}</button>
+  </main>
+</template>
